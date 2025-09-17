f11191bba1a7ba42b2c968d7aa8996f3cf19a95c
chloe-ek <chloe.kwon08@gmail.com>
2025-09-17 16:49:32 -0700

    refactor: convert TypeScript namespaces to ES2015 modules

--- electron/preload/preload.ts
+++ electron/preload/preload.ts
@@ -1,7 +1,7 @@
 import { contextBridge, ipcRenderer, shell } from "electron";
 import * as path from "path";
 import IpcChannel from "../common/ipc/IpcChannel";
-import Ipc from "../common/ipc/IpcHandler";
+import * as Ipc from "../common/ipc/IpcHandler";
 import { setListener } from "./ipcRendererUtils";
 
 // This file controls access to the Electron and Node methods required by the renderer process
@@ -11,46 +11,51 @@ export const api = {
   joinPath: (...paths: string[]) => path.join(...paths),
   normalizePath: (filePath: string) => path.normalize(filePath),
   ipcToMain: {
-    appVersion: (): Ipc.AppVersion.Response => ipcRenderer.invoke(IpcChannel.APP_VERSION),
+    appVersion: (): Ipc.AppVersionResponse => ipcRenderer.invoke(IpcChannel.APP_VERSION),
 
-    checkCameraAccess: (): Ipc.CheckCameraAccess.Response =>
+    checkCameraAccess: (): Ipc.CheckCameraAccessResponse =>
       ipcRenderer.invoke(IpcChannel.CHECK_CAMERA_ACCESS),
 
-    getUserPreferences: (): Ipc.GetUserPreferences.Response =>
+    getUserPreferences: (): Ipc.GetUserPreferencesResponse =>
       ipcRenderer.invoke(IpcChannel.GET_USER_PREFERENCES),
 
-    logRenderer: (payload: Ipc.LogRenderer.Payload): Ipc.LogRenderer.Response =>
+    logRenderer: (payload: Ipc.LogRendererPayload): Ipc.LogRendererResponse =>
       ipcRenderer.invoke(IpcChannel.LOG_RENDERER, payload),
 
     saveSettingsAndClose: (
-      payload: Ipc.SaveSettingsAndClose.Payload
-    ): Ipc.SaveSettingsAndClose.Response =>
+      payload: Ipc.SaveSettingsAndClosePayload
+    ): Ipc.SaveSettingsAndCloseResponse =>
       ipcRenderer.invoke(IpcChannel.SAVE_SETTINGS_AND_CLOSE, payload),
 
-    openUserDataDirectory: (): Ipc.OpenUserDataDirectory.Response =>
+    openUserDataDirectory: (): Ipc.OpenUserDataDirectoryResponse =>
       ipcRenderer.invoke(IpcChannel.OPEN_APP_DATA_DIRECTORY),
 
-    openConfirmPrompt: (payload: Ipc.OpenConfirmPrompt.Payload): Ipc.OpenConfirmPrompt.Response =>
+    openConfirmPrompt: (payload: Ipc.OpenConfirmPromptPayload): Ipc.OpenConfirmPromptResponse =>
       ipcRenderer.invoke(IpcChannel.OPEN_CONFIRM_PROMPT, payload),
 
-    openDirDialog: (payload: Ipc.OpenDirDialog.Payload): Ipc.OpenDirDialog.Response =>
+    openDirDialog: (payload: Ipc.OpenDirDialogPayload): Ipc.OpenDirDialogResponse =>
       ipcRenderer.invoke(IpcChannel.OPEN_DIR_DIALOG, payload),
 
     openExportVideoFilePathDialog: (
-      payload: Ipc.OpenExportVideoFilePathDialog.Payload
-    ): Ipc.OpenExportVideoFilePathDialog.Response =>
+      payload: Ipc.OpenExportVideoFilePathDialogPayload
+    ): Ipc.OpenExportVideoFilePathDialogResponse =>
       ipcRenderer.invoke(IpcChannel.OPEN_EXPORT_VIDEO_FILE_PATH_DIALOG, payload),
 
-    exportVideoStart: (payload: Ipc.ExportVideoStart.Payload): Ipc.ExportVideoStart.Response =>
+    exportVideoStart: (payload: Ipc.ExportVideoStartPayload): Ipc.ExportVideoStartResponse =>
       ipcRenderer.invoke(IpcChannel.EXPORT_VIDEO_START, payload),
 
-    showItemInFolder: (payload: Ipc.ShowItemInFolder.Payload): Ipc.ShowItemInFolder.Response =>
+    showItemInFolder: (payload: Ipc.ShowItemInFolderPayload): Ipc.ShowItemInFolderResponse =>
       ipcRenderer.invoke(IpcChannel.SHOW_ITEM_IN_FOLDER, payload),
+
+    copyFramesToTempDirectory: (
+      payload: Ipc.CopyFramesToTempDirectoryPayload
+    ): Ipc.CopyFramesToTempDirectoryResponse =>
+      ipcRenderer.invoke(IpcChannel.COPY_FRAMES_TO_TEMP_DIRECTORY, payload),
   },
   ipcToRenderer: {
-    onCloseButtonClick: (callback: (payload: Ipc.OnCloseButtonClick.Payload) => void) =>
+    onCloseButtonClick: (callback: (payload: Ipc.OnCloseButtonClickPayload) => void) =>
       setListener(IpcChannel.ON_CLOSE_BUTTON_CLICK, callback),
-    onExportVideoData: (callback: (payload: Ipc.OnExportVideoData.Payload) => void) =>
+    onExportVideoData: (callback: (payload: Ipc.OnExportVideoDataPayload) => void) =>
       setListener(IpcChannel.ON_EXPORT_VIDEO_DATA, callback),
   },
   openExternal: {
